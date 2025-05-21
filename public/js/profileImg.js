ecea9f2f26647d6a5c14a8e80ca0664d90430d12
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2025-05-21 23:04:24 +0900

    画像の表示形式を変更

--- public/js/profileImg.js
+++ public/js/profileImg.js
@@ -1,14 +1,23 @@
 const fileInput = document.getElementById('profile-img-fileInput');
 const preview = document.getElementById('profile-img-edit');
+const imgFluidDummy= document.getElementById('img-fluid-dummy');
 
 fileInput.addEventListener('change', (event) => {
 const file = event.target.files[0];
 if (!file) return;
 
-const reader = new FileReader();
-reader.onload = (e) => {
-    preview.src = e.target.result; // 選んだ画像をプレビューにセット
-};
-reader.readAsDataURL(file);
+ // 画像かどうかをチェック
+    if (file.type.startsWith('image/')) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            preview.src = e.target.result; // 選んだ画像をプレビューにセット
+        };
+        reader.readAsDataURL(file);
+        preview.classList.remove('none');
+        imgFluidDummy.classList.add('none');
+    }else{
+        imgFluidDummy.classList.remove('none');
+        preview.classList.add('none');
+    }
 });
 
